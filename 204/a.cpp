23e2b0eea86980766562c7c6dc25dea895b6520e
Felix Fung <felix.the.cheshire.cat@gmail.com>
2013-01-22 04:15:00 -0800

    Editorial Day

--- 204/a.cpp
+++ 204/a.cpp
@@ -1,40 +1,64 @@
+// Read another editorial. Today is a learning day.
+
 #include <iostream>
-#include <vector>
+
+#define ull unsigned long long
 
 using namespace std;
 
-#define ull unsigned long long
+ull logten(ull x) {
+  if (x < 10) {
+    return 0;
+  }
+  return 1 + logten(x / 10);
+}
+
+ull pow(ull x, ull k) {
+  if (k == 0) {
+    return 1;
+  }
+  return x * pow(x, k - 1);
+}
+
+ull F(ull x) {
+  if (x < 10) {
+    return x;
+  }
+
+  ull lg = logten(x);
+  ull digs = lg + 1;
+  ull up = pow(10, lg);
+  ull firstdigit = x / up;
+  ull lastdigit = x % 10;
+  ull xp = (x - ((x / up) * up)) / 10;
+
+  ull ans = 0;
+
+  for (int i = 1; i <= digs; i++) {
+    for (int d = 1; d <= 9; d++) {
+      if (i == digs && d == firstdigit) {
+        ans += xp;
+        if (lastdigit >= d) {
+          ans += 1;
+        }
+      } else if (i == digs && d < firstdigit) {
+        ans += pow(10, i - 2);
+      } else if (i < digs) {
+        if (i == 1) {
+          ans++;
+        } else {
+          ans += pow(10, i - 2);
+        }
+      }
+    }
+  }
+
+  return ans;
+}
 
 int main() {
   ull l, r;
   cin >> l >> r;
 
-  ull cnt = 0;
-  for (int i = 1; i <= 9; i++) {
-    if (i >= l && i <= r) {
-      cnt++;
-    }
-  }
-
-  ull pow = 1;
-  for (int i = 1; i <= 18; i++) {
-    pow *= 10;
-    for (int j = 1; j <= 9; j++) {
-      ull bot = j * pow + j;
-      ull top = j * pow + (pow - 10) + j;
-
-      // no intersection
-      if (l > top || r < bot) {
-        continue;
-      }
-
-      if (bot >= l && top <= r) { // contained
-        cnt += pow / 10;
-      } else if (l >= bot && r <= top) { // other contained
-      }
-
-      //cout << bot << " to " << top << endl;
-    }
-  }
-
+  cout << F(r) - F(l - 1) << endl;
 }
